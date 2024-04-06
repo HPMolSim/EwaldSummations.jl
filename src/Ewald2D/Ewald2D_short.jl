e7a23e012168bb76f5339392277309c472691669
Xuanzhao Gao <gaoxuanzhao@gmail.com>
2024-04-06 18:19:06 +0800

    update

--- src/Ewald2D/Ewald2D_short.jl
+++ src/Ewald2D/Ewald2D_short.jl
@@ -1,22 +1,22 @@
 function Ewald2D_short_energy(interaction::Ewald2DInteraction{T}, neighbor::CellList3D{T}, position::Vector{Point{3, T}}, q::Vector{T}) where{T}
     neighbor_list = neighbor.neighbor_list
 
-    energy_short = zero(T)
     boundary = Q2dBoundary(interaction.L[1], interaction.L[2], interaction.L[3])
 
-    for (i, j, ρ) in neighbor_list
+    energy_short = @distributed (+) for (i, j, ρ) in neighbor_list
         coord_1, coord_2, r_sq = position_check3D(position[i], position[j], boundary, interaction.r_c)
         if iszero(r_sq)
-            nothing
+            E = zero(T)
         else
             q_1 = q[i]
             q_2 = q[j]
-            energy_short += Ewald2D_Es_pair(q_1, q_2, interaction.α, r_sq)
+            E = Ewald2D_Es_pair(q_1, q_2, interaction.α, r_sq)
         end
+        E
     end
 
-    for i in 1:interaction.n_atoms
-        energy_short += Ewald2D_Es_self(q[i], interaction.α)
+    energy_short += @distributed (+) for i in 1:interaction.n_atoms
+        Ewald2D_Es_self(q[i], interaction.α)
     end
 
     return energy_short / (4π * interaction.ϵ)
